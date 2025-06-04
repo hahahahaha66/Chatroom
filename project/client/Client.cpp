4407dfbfe009a4e69a8a695ff10bf93fce71db2c
hahaha <a310608318@gmail.com>
2025-06-04 20:22:09 +0800

    补充交互界面

--- project/client/Client.cpp
+++ project/client/Client.cpp
@@ -191,15 +191,33 @@ void Client::PrivateChatInterface(int friendid)
         
         if (order_ == '1')
         {
-            
+            //检查是否屏蔽，如果屏蔽则打印并显示已屏蔽，无法发送消息
+            std::string message;
+            std::cout << "输入消息 : ";
+            std::cin >> message;
+            //~
+            //将消息发送给朋友
         }
         else if (order_ == '2')
         {
-
+            //~
+            //这里有两种情况
+            //若未屏蔽，则设为屏蔽
+            //若已屏蔽，则设为未屏蔽
+            //将最终结果打印出来
         }
         else if (order_ == '3')
         {
-
+            int temp_order;
+            std::cout << "1. 确认删除好友" << std::endl;
+            std::cout << "2. 取消删除好友" << std::endl;
+            std::cin >> temp_order;
+            if (temp_order == 1)
+            {
+                //~
+                //删除好友，并返回上一界面
+                break;
+            }
         }
         else if (order_ == 'q')
         {
@@ -208,6 +226,166 @@ void Client::PrivateChatInterface(int friendid)
         else  
         {
             std::cout << "命令输入错误,请重新输入" << std::endl;
+        }
+    }
+}
+
+void Client::GroupChatInterface(int groupid)
+{
+    while (true)
+    {
+        char order_;
+
+        if ()  //普通用户
+        {
+            std::cout << "-----------输入q退出或返回上一界面----------------" << std::endl;
+            std::cout << "1. 发送消息" << std::endl;
+            std::cout << "2. 查看群聊成员" << std::endl;
+            std::cout << "3. 退出群聊" << std::endl;
+        }
+        else if ()  //管理员
+        {
+            std::cout << "-----------输入q退出或返回上一界面----------------" << std::endl;
+            std::cout << "1. 发送消息" << std::endl;
+            std::cout << "2. 查看群聊成员" << std::endl;
+            std::cout << "3. 退出群聊" << std::endl;
+            std::cout << "4. 查看申请列表" << std::endl;
+            std::cout << "5. 移除成员" << std::endl;
+        
+        }
+        else  //群主
+        {
+            std::cout << "-----------输入q退出或返回上一界面----------------" << std::endl;
+            std::cout << "1. 发送消息" << std::endl;
+            std::cout << "2. 查看群聊成员" << std::endl;
+            std::cout << "3. 退出群聊" << std::endl;
+            std::cout << "4. 查看申请列表" << std::endl;
+            std::cout << "5. 移除成员" << std::endl;
+            std::cout << "6. 设置管理员" << std::endl;
+            std::cout << "7. 取消管理员" << std::endl;
+            std::cout << "8. 删除群聊" << std::endl;
+        }
+
+        std::cin >> order_;
+
+        if (order_ == '1')
+        {
+            std::string message;
+            std::cout << "输入消息 : " << std::endl;
+            std::cin >> message;
+            //~
+            //发送消息群聊
+        }
+        else if (order_ == '2')
+        {
+            //~
+            //打印群聊成员
+        }
+        else if (order_ == '3')
+        {
+            char sure;
+            std::cout << "1. 确定退出群聊" << std::endl;
+            std::cout << "2. 取消退出群聊" << std::endl;
+            std::cin >> sure;
+
+            if (sure == '1')
+            {
+                //~
+                //推出群聊
+                break;
+            }
+            else if (sure == '1' || sure == 'q')
+            {
+                continue;
+            }
+            else  
+            {
+                std::cout << "输入错误,自动返回上一界面" << std::endl;
+            }
+        }
+        else if (order_ == '4')
+        {
+            //~
+            //打印申请列表
+            char apply_order;
+            std::cout << "1. 输入批准的成员id" << std::endl;
+            std::cout << "2. 返回上一界面" << std::endl;
+            std::cin >> apply_order;
+
+            if (apply_order == '1')
+            {
+
+            }
+            else if (apply_order == '2' || apply_order == 'q')
+            {
+                continue;
+            }
+            else  
+            {
+                std::cout << "输入错误,自动返回上一界面" << std::endl;
+            }
+        }
+        else if (order_ == '5')
+        {
+            //~
+            //打印成员列表
+            int removeid = 0;
+            std::cout << "输入移除的成员id : " << std::endl;
+            //检查是否是成员id
+            //是则移除
+            //不是则报错
+        }
+        else if (order_ == '6')
+        {
+            int new_administratorid = 0;
+            //~
+            //打印群聊成员列表
+            std::cout << "输入新的管理员id : ";
+            std::cin >> new_administratorid;
+            //判断是否是群聊成员
+            //~
+            //执行设置管理员
+        }
+        else if (order_ == '7')
+        {
+            int old_administratorid = 0;
+            //~
+            //打印管理员员列表
+            std::cout << "输入取消的管理员id : ";
+            std::cin >> old_administratorid;
+            //判断是否是管理员员
+            //~
+            //执行取消管理员
+        }                        
+        else if (order_ == '8')
+        {
+            char sure;
+            std::cout << "1. 确定删除群聊" << std::endl;
+            std::cout << "2. 取消删除群聊" << std::endl;
+            std::cin >> sure;
+
+            if (sure == '1')
+            {
+                //~
+                //删除群聊
+                break;
+            }
+            else if (sure == '1' || sure == 'q')
+            {
+                continue;
+            }
+            else  
+            {
+                std::cout << "输入错误,自动返回上一界面" << std::endl;
+            }
+        }        
+        else if (order_ == 'q')
+        {
+            break;
+        }
+        else  
+        {
+            std::cout << "命令错误,请重新输入" << std::endl;
         }
     }
 }
