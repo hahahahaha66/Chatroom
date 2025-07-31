8b8c2032c7b909d3a6601da532863eb58561b00d
hahaha <a310608318@gmail.com>
2025-07-31 18:02:16 +0800

    7.31

--- new_project/main/main_client.cpp
+++ new_project/main/main_client.cpp
@@ -1,6 +1,7 @@
 #include "../client/Client.h"
 
 #include <gperftools/profiler.h>
+#include <csignal>
 
 namespace {
     std::function<void()> g_handler;  // 不暴露 loop，仅暴露行为
@@ -9,7 +10,7 @@ namespace {
 void Quit(int sig)
 {
     std::cout << "检测到信号Ctrl + C" << std::endl;
-    ProfilerStop();
+    // ProfilerStop();
 
     if (g_handler) {
         g_handler();  // 间接调用含有 loop 的 lambda
@@ -18,14 +19,14 @@ void Quit(int sig)
 
 int main ()
 {
-    ProfilerStart("cpu_profile.prof");
+    // ProfilerStart("cpu_profile.prof");
     EventLoop loop;
 
     g_handler = [&loop]() {
         loop.quit();
         loop.wakeup();
     };
-    // signal(SIGINT, Quit);
+    signal(SIGINT, Quit);
 
     Client client(loop, 10101, "10.30.0.120", "ChatClient");
 
